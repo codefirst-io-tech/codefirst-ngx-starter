8f21fe8b371bd21c1065d796d36538f7ab8837d6
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2022-01-26 14:26:03 +0300

    chore: button component css changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'immino-green': 'rgb(16,188,73)',
+        'immino-blue': 'rgb(18,104,253)',
+        'immino-red': 'rgb(234,34,47)',
+        'immino-yellow': 'rgb(253,193,12)',
+      }
+    },
   },
   variants: {
     extend: {}
   },
-  plugins: []
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography')
+  ]
 };
